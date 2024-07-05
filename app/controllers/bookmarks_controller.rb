d51b95328b0787743db6a065bd1790e6a92a5f13
Loic CARPENTIER <carpentier.loic19@gmail.com>
2024-07-05 11:46:38 +0200

    Fix database configuration

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,28 +1,36 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+  before_action :set_bookmark, only: [:destroy]
+
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
-    @list = List.find(params[:list_id])
-    @bookmark = @list.bookmarks.build(bookmark_params)
+    @bookmark = @list.bookmarks.new(bookmark_params)
     if @bookmark.save
-      redirect_to @list
+      redirect_to @list, notice: 'Bookmark was successfully created.'
     else
       render :new
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@bookmark.list)
+    redirect_back fallback_location: lists_path, notice: 'Bookmark was successfully destroyed.'
   end
 
   private
 
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id)
+    params.require(:bookmark).permit(:comment, :movie_id, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :lists do
-    resources :bookmarks, only: [:new, :create, :destroy]
+    resources :bookmarks, only: [:new, :create]
     resources :reviews, only: [:create, :destroy]
   end
+
+  resources :bookmarks, only: [:destroy] # Ajoute cette ligne pour les routes non imbriquées
   root to: 'lists#index'
 end
